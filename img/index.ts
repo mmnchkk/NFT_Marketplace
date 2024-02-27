f66eecf83e72aafb5c177522d08fc130e290b917
mmnchkk <152281018+mmnchkk@users.noreply.github.com>
2024-02-27 19:45:17 +0500

    Add files via upload

---
+++ img/index.ts
@@ -0,0 +1,62 @@
+import Logo from './imgHeader/logo.svg'
+import User from "./imgHeader/User.svg"
+
+import RocketImg from "./imgIntro/rocket.svg"
+import SpaceBackground from "./imgIntro/spaceWalking.svg"
+import SpaceMen from "./imgIntro/space-men.svg"
+
+import DSGNImg from "./imgTrending/PrimarPhotoPlaceholder.svg"
+import DSGNImgTwo from "./imgTrending/SecondarPhotoPlaceholder.svg"
+import DSGNImgThree from "./imgTrending/SecondaryPhotoPlaceholder.svg"
+import ImgAny from "./imgTrending/NumberofadditionalNFTs.svg"
+import IconOne from "./imgTrending/Asset.svg"
+import IconTwo from "./imgTrending/AssetTwo.svg"
+import IconThree from "./imgTrending/AvatarThree.svg"
+import MagicImg from "./imgTrending/PrimaryPhotoPlaceholde.svg"
+import MagicImgTwo from "./imgTrending/SeondaryPhotoPlaceholder.svg"
+import MagicImgThree from "./imgTrending/SecondaryPhotoPlaeholder.svg"
+import DiscoImg from "./imgTrending/PrimaryPhotPlaceholder.svg"
+import DiscoImgTwo from "./imgTrending/SecondaryPhotoPlceholder.svg"
+import DiscoImgThree from "./imgTrending/SecondarPhotoPlacehlder.svg"
+
+import IconRocket from "./imgTopCreators/violet-rocket.svg"
+
+import Distant from "./imgDiscoverMore/Distant.svg"
+import Life from "./imgDiscoverMore/Life.svg"
+import AstroFiction from "./imgDiscoverMore/AstroFiction.svg"
+import SeeAll from "./imgDiscoverMore/Eye.svg"
+import MoonDancer from "./imgDiscoverMore/MoonDancer.svg"
+import NebulaKid from "./imgDiscoverMore/NebulaKid.svg"
+import SpaceOne from "./imgDiscoverMore/Spaceone.svg"
+
+import UserMushrooms from "./imgMagicMushrooms/user.svg"
+
+import Wallet from "./imgHowItWorks/wallet.svg"
+import Collection from "./imgHowItWorks/Collection.svg"
+import StartEarning from "./imgHowItWorks/StartEarning.svg"
+import Astronaut from "./imgHowItWorks/Astronaut.svg"
+
+import Cosmo from "./imgConnect/back.svg"
+import Metamask from "./imgConnect/Metamask.svg"
+import WalletConnect from "./imgConnect/WalletConnect.svg"
+import Coinbase from "./imgConnect/Coinbase.svg"
+
+import MagnifyingGlass from "./imgMarketplace/MagnifyingGlass.svg"
+
+import CosmoTwo from "./imgCreateAcc/back.svg"
+
+export { 
+    Logo, User,
+    RocketImg, SpaceBackground, SpaceMen, 
+    DSGNImg, DSGNImgTwo, DSGNImgThree, ImgAny, IconOne, IconTwo, IconThree, MagicImg, MagicImgTwo, MagicImgThree, DiscoImg, DiscoImgTwo, DiscoImgThree,
+    IconRocket, 
+    Distant, Life, AstroFiction, SeeAll ,MoonDancer, NebulaKid, SpaceOne, 
+    UserMushrooms,
+    Wallet ,Collection, StartEarning, Astronaut,
+
+    Cosmo, Metamask, WalletConnect, Coinbase,
+
+    MagnifyingGlass,
+
+    CosmoTwo,
+}
